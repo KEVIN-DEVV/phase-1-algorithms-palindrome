23dc055b20871218f46481de073c480ba0784a69
KEVIN-DEVV <kyevenkims@gmail.com>
2022-12-12 20:50:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const stringArray = word.split('');
+  const reversedstringArray = stringArray.reverse();
+  const reversedWord = reversedstringArray.join('');
+
+  if (reversedWord === word) {
+    return true;
+
+}else{
+  return false;
 }
+
 
 /* 
   Add your pseudocode here
